0cf63c5f210ef2ec796bb7ea3f55425b4c978d23
pianfei <lanxian@pianfei.com>
2025-02-13 23:07:49 +0800

    代码

---
+++ daima/code1/src/main.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : code1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World\n");
+    return 0;
+}
